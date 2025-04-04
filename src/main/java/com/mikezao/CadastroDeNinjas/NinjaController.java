ee59694869b8bf0e7db91242d12c8e13dd45b79a
Mcf11 <michel.felipe0@hotmail.com>
2025-04-04 18:29:46 -0300

    Modelo para banco de dados, JPA dependency

---
+++ src/main/java/com/mikezao/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,17 @@
+package com.mikezao.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/hello")
+    public String boasVindas(){
+        return "Hello World!";
+    }
+
+
+}
